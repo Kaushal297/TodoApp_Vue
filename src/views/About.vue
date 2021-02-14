20ffdc9741ae3bdbf9494eb9774d9504208e52c2
Kaushal <kaushalkumar297@gmail.com>
2021-02-14 07:44:16 +0530

    missed files Added

---
+++ src/views/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <p>Hi, My name is Kaushal and this is My first Vue WebApp named Todo. It is a part of the crash course i did it on Vue JS on 14 Feb 2021.</p>
+    <p><strong>Youtube</strong> : <a href="https://www.youtube.com/watch?v=Wy9q22isx3U">Click Here </a> if you want to do the same.</p>
+    <p><strong>Github</strong> : <a href="https://github.com/Kaushal297/TodoApp_Vue">link</a> </p>
+    <p><strong>Version 1.0.0</strong></p>
+  </div>
+</template>
+
+<style scoped>
+.about {
+  margin-top: 10px;
+  text-align: center;
+}
+
+p{
+  line-height: 1.4;
+}
+</style>
